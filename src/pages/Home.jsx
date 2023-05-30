27285b9b46fd666b2295f9b18273fbf5b22254a2
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2023-05-30 11:02:07 +0530

    added search box for searching and added api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react"
 
 const Home = () => {
+  const [inputValue,setInputValue]=useState('');
+
+  const onInputChange=(ev)=>{
+      setInputValue(ev.target.value);
+  }
+
+  const onSearch = async (ev)=>{
+    ev.preventDefault();
+
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`)
+    const body = await response.json()
+      console.log(body);
+  }
+
   return (
     <div>
-        Homme
+    <form onSubmit={onSearch}>
+        <input type="text" value={inputValue} onChange={onInputChange}/>
+        <button type="submit">Search</button>
+    </form>
     </div>
   )
 }
